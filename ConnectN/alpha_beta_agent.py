f8238c2cee2d4df66f4d4726c6c2bc483c528df2
Joseph Yuen <josephnewbox@gmail.com>
2020-02-10 12:46:57 -0500

    revised utility function based on what we discussed. however, don't know if the helper functions work yet

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -72,23 +72,30 @@ class AlphaBetaAgent(agent.Agent):
         points = 0
         for h in range(brd.h):
             for w in range(brd.w):
+                tempPoints = 0
                 piece = brd.board[h][w]
-                if piece == 2 and w == 0:
-                    points += 10
-                elif piece == 2 and w == 1:
-                    points += 7
-                elif piece == 2 and w == 2:
-                    points += 5
-                elif piece == 2 and w == 3:
-                    points += 2
-                elif piece == 2 and w == 4:
-                    points -= 1
-                elif piece == 2 and w == 5:
-                    points -= 5
-                elif piece == 2 and w == 6:
-                    points -= 10
+
+                # Check for 2,3,4,...,n
+                for n in range(brd.n,1,-1):
+                    if piece == 1: # Player 1
+                        # Tentative Values - Mei please don't kill me
+                        # 2 - +1
+                        # 3 - +2
+                        # 4 - +3
+                        # n - +(n-1)
+                        tempPoints = tempPoints + (brd.is_any_n_line_at(h,w,n) * (n-1))
+                    else: # Player 2
+                        # Tentative Values - Winny please don't kill me
+                        # 2 - -1
+                        # 3 - -2
+                        # 4 - -3
+                        # n - -(n-1)
+                        tempPoints = tempPoints + (brd.is_any_n_line_at(h,w,n) * (n-1) * -1)
+
+                points = points + tempPoints
         # print("Points :", points)
         # brd.print_it()
+        print(points)
         return points
         # return random.randint(0, 100)
 
@@ -117,3 +124,39 @@ class AlphaBetaAgent(agent.Agent):
             # Add board to list of successors
             succ.append((nb, col))
         return succ
+
+
+    # Check if a line of identical tokens exists starting at (x,y) in direction (dx,dy)
+    #
+    # PARAM [int] x:  the x coordinate of the starting cell
+    # PARAM [int] y:  the y coordinate of the starting cell
+    # PARAM [int] dx: the step in the x direction
+    # PARAM [int] dy: the step in the y direction
+    # RETURN [Bool]: True if n tokens of the same type have been found, False otherwise
+    def is_line_at(self, x, y, dx, dy, n):
+        """Return True if a line of identical tokens exists starting at (x,y) in direction (dx,dy)"""
+        # Avoid out-of-bounds errors
+        if ((x + (self.n-1) * dx >= self.w) or
+            (y + (self.n-1) * dy < 0) or (y + (self.n-1) * dy >= self.h)):
+            return False
+        # Get token at (x,y)
+        t = self.board[y][x]
+        # Go through elements
+        for i in range(1, self.n):
+            if self.board[y + i*dy][x + i*dx] != t:
+                return False
+        return True
+
+    # Check if a line of identical tokens exists starting at (x,y) in any direction
+    #
+    # PARAM [int] x:  the x coordinate of the starting cell
+    # PARAM [int] y:  the y coordinate of the starting cell
+    # RETURN [Bool]: True if n tokens of the same type have been found, False otherwise
+    def is_any_n_line_at(self, x, y, n):
+        """Return True if a line of identical tokens exists starting at (x,y) in any direction"""
+        
+        # return number of lines that satisfy the given number...
+        return (self.is_line_at(x, y, 1, 0, n) or # Horizontal
+                self.is_line_at(x, y, 0, 1, n) or # Vertical
+                self.is_line_at(x, y, 1, 1, n) or # Diagonal up
+                self.is_line_at(x, y, 1, -1, n)) # Diagonal down
