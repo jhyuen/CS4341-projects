c33f5a3a83e0b2d1282b9bb9039624d4aa161417
Joseph Yuen <josephnewbox@gmail.com>
2020-02-11 13:16:17 -0500

    don't kill me mei

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -30,13 +30,15 @@ class AlphaBetaAgent(agent.Agent):
         # Make decision
         curdepth = 0
         v, a = self.maxval(brd, -math.inf, math.inf, curdepth)
-        print("V and action: ", v, a)
-        return a                  # fix this later (arg(v))
+        print("Points: " + str(v))
+        print("Action: " + str(a))
+
+        return a
 
     # Return maximum utility across all nodes
     def maxval(self, brd, alpha, beta, curdepth):
         if curdepth == self.max_depth:
-            return self.utility(brd), -1               # fix this
+            return self.utility(brd), -1               
         v = -math.inf
         action = -1
         brdactlist = self.get_successors(brd)
@@ -71,33 +73,21 @@ class AlphaBetaAgent(agent.Agent):
     def utility(self, brd):
         points = 0
 
-        print('h: '+ str(brd.h))
-        print('w: '+ str(brd.w))
+        player = 1
+        opponent = 2
+
+        if brd.player == 1:
+            player == brd.player
+            opponent == 2
+        else:
+            player = brd.player
+            opponent = 1
+
         for r in range(brd.h):
             for c in range(brd.w):
-                tempPoints = 0
-                piece = brd.board[r][c]
-                if piece != 0:
-                    print('piece: ' + str(piece))
-                
-                # Check for 2,3,4,...,n
-                for n in range(brd.n,1,-1):
-                    if piece != 0:
-                        print('n: ' + str(n))
-                        print('r: ' + str(r) + ' c: ' + str(c))
+                points += self.checkLines(brd,c,r,brd.n,player,opponent)
 
-                    if piece == 1: # Player 1
-                        tempPoints += (self.is_any_n_line_at(brd,c,r,n,1) * (n-1))
-                    else: # Player 2
-                        tempPoints += (self.is_any_n_line_at(brd,c,r,n,2) * (n-1) * -1)
-
-                points = points + tempPoints
-                
-        # print("Points :", points)
-        # brd.print_it()
-        print('points: ' + str(points))
         return points
-        # return random.randint(0, 100)
 
 
     # Get the successors of the given board.
@@ -133,22 +123,46 @@ class AlphaBetaAgent(agent.Agent):
     # PARAM [int] dx: the step in the x direction
     # PARAM [int] dy: the step in the y direction
     # PARAM [int] p: player token number
-    # RETURN [Bool]: True if n tokens of the same type have been found, False otherwise EDIT
-    def is_n_line_at(self, brd, x, y, dx, dy, n, p):
-        """Return True if a line of identical tokens exists starting at (x,y) in direction (dx,dy)"""
+    # RETURN [int]: True if n tokens of the same type have been found, False otherwise EDIT
+    def connectNCheck(self, brd, x, y, dx, dy, n, p, o):
+        """ """
         # Avoid out-of-bounds errors
         if ((x + (n-1) * dx >= brd.w) or
             (y + (n-1) * dy < 0) or (y + (n-1) * dy >= brd.h)):
-            return False
+            return 0
+
         # Get token at (x,y)
         # print(str(x)+ ' ' + str(y))
-        #t = brd.board[y][x]
 
-        # Go through elements
-        for i in range(1, n):
-            if brd.board[y + i*dy][x + i*dx] != p:
-                return False
-        return True
+        playerTokens = 0
+        opponentTokens = 0
+        spaceTokens = 0
+
+        # piece  = brd.board[y][x]
+        for i in range(0, n):
+            target = brd.board[y + i*dy][x + i*dx]
+            if target == p:
+                playerTokens += 1
+            elif target == o:
+                opponentTokens += 1
+            else:
+                spaceTokens += 1
+        
+        # Evaluate line
+        if spaceTokens == n:
+            return 0
+        elif playerTokens == n:
+            return 100000
+        elif opponentTokens == n:
+            return -100000
+        elif (playerTokens > 0) and (opponentTokens > 0):
+            return 0
+        elif (playerTokens > 0) and (opponentTokens == 0):
+            return playerTokens*n
+        elif (opponentTokens > 0) and (playerTokens == 0):
+            return opponentTokens*n*-1
+        else:
+            return 0
 
     # Check if a line of identical tokens exists starting at (x,y) in any direction
     #
@@ -158,18 +172,12 @@ class AlphaBetaAgent(agent.Agent):
     # PARAM [int] n: the number of tokens in a row
     # PARAM [int] p: player token number
     # RETURN [Bool]: True if n tokens of the same type have been found, False otherwise EDIT
-    def is_any_n_line_at(self, brd, x, y, n, p):
-        """ Return number of lines of identical tokens starting at (x,y) 
-            in any direction that satisfy the given number """
-        lines = 0
-        if self.is_n_line_at(brd, x, y, 1, 0, n, p): # Horizontal
-            lines += 1
-        if self.is_n_line_at(brd, x, y, 0, 1, n, p): # Vertical
-            lines += 1
-        if self.is_n_line_at(brd, x, y, 1, 1, n, p): # Diagonal up
-            lines += 1
-        if self.is_n_line_at(brd, x, y, 1, -1, n, p): # Diagonal down
-            lines += 1
-
-        return lines
+    def checkLines(self, brd, x, y, n, p, o):
+        """ """
+        points = 0
+        points += self.connectNCheck(brd, x, y, 1, 0, n, p, o) # Horizontal
+        points += self.connectNCheck(brd, x, y, 0, 1, n, p, o) # Vertical
+        points += self.connectNCheck(brd, x, y, 1, 1, n, p, o) # Diagonal up
+        points += self.connectNCheck(brd, x, y, 1, -1, n, p, o) # Diagonal down
+        return points
             
